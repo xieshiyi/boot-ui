43d720fd463cb51ca5a733364a596cff0aa8bcb3
sherry <xieshiyi@xieshiyis-MacBook-Pro.local>
2016-09-14 12:38:51 +0800

    first commit

--- register.js
+++ register.js
@@ -1,11 +1,28 @@
 $(function(){
-  var mobile=$('#tel').val();
-  var nick=$('#name').val();
-  var email=$('#email').val();
-  var password=$('#password').val();
+
+  new Vue({
+    el:'#reg_content',
+    data:{
+      items:[
+        {name:'手机号：'},
+        {name:'验证码：'},
+        {name:'手机验证码：'},
+        {name:'用户名：'},
+        {name:'常用邮箱：'},
+        {name:'输入密码：'}
+      ]
+    }
+  });
+
+
+
+  var mobile=$('input').eq(0).val();
+  var nick=$('input').eq(3).val();
+  var email=$('input').eq(4).val();
+  var password=$('input').eq(5).val();
   $('#btn_reg').click(function(event) {
     /* Act on the event */
-  
+
     $.ajax({
           url: "http://192.168.0.4:8080/api/users/register",
           type:'post',
@@ -20,5 +37,6 @@ $(function(){
           }
         });
   });
+
 
 });
